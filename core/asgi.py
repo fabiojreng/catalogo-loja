7df76ff8c678542257ca17d088a7abccaf5f3bee
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-01-04 17:25:13 -0300

    upgrade

--- core/asgi.py
+++ core/asgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+tmp_media_path = "/media"
+if not os.path.exists(tmp_media_path):
+    os.makedirs(tmp_media_path)
+
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_asgi_application()
