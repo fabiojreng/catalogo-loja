6911034aa508ccacbaac10ff78f3a44bab80dab5
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-01-04 17:30:33 -0300

    update

--- core/settings.py
+++ core/settings.py
@@ -127,5 +127,6 @@ STATIC_ROOT = os.path.join("staticfiles")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-MEDIA_ROOT = os.path.join(BASE_DIR, "tmp/media")
+# Configuração do diretório de mídia
 MEDIA_URL = "/media/"
+MEDIA_ROOT = "/tmp/media"  # Diretório temporário na Vercel
