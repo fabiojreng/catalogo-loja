05df964c82590ed776de29a6bc92d7c3a92bff00
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-01-04 15:48:56 -0300

    update

--- core/settings.py
+++ core/settings.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "catalogo",
+        "NAME": "catalogo_lufashion",
         "USER": os.getenv("DATABASE_USER"),
         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
         "HOST": os.getenv("DATABASE_HOST"),
