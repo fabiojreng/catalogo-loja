115de42eee61731ea68caea533db21fbaa800432
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-01-04 16:21:00 -0300

    update

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("product/list/", views.ProductListView.as_view(), name="product_list"),
+    path("", views.ProductListView.as_view(), name="product_list"),
     path(
-        "product/<int:pk>/details/",
+        "<int:pk>/detalhes/",
         views.ProductDetailView.as_view(),
         name="product_details",
     ),
--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+
+def home_redirect(request):
+    return redirect("product_list")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("catalog.urls")),
+    path("", home_redirect, name="home"),
+    path("produtos/", include("catalog.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
