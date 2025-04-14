3d5f3c62130dd67e507b6044e99d2732482aa458
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-04-14 17:02:11 -0300

    update

--- administrators/forms.py
+++ administrators/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from catalog.models import Category, Product
+from catalog.models import Category, Product, ProductImage
+from django.forms import inlineformset_factory
 
 
 class CategoryForm(forms.ModelForm):
@@ -11,4 +12,29 @@ class CategoryForm(forms.ModelForm):
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "description", "value", "image", "category"]
+        fields = ["name", "description", "value", "category", "image"]
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "description": forms.TextInput(attrs={"class": "form-control"}),
+            "value": forms.NumberInput(attrs={"class": "form-control"}),
+            "category": forms.Select(attrs={"class": "form-control"}),
+            "image": forms.ClearableFileInput(attrs={"class": "form-control"}),
+        }
+
+
+class ProductImageForm(forms.ModelForm):
+    class Meta:
+        model = ProductImage
+        fields = ["image", "description"]
+        labels = {"image": "Imagem"}
+        widgets = {
+            "image": forms.ClearableFileInput(attrs={"class": "form-control"}),
+            "description": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Descrição da imagem"}
+            ),
+        }
+
+
+ProductImageFormSet = inlineformset_factory(
+    Product, ProductImage, form=ProductImageForm, extra=1, can_delete=True
+)
--- administrators/views.py
+++ administrators/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from catalog.models import Category, Product
-from .forms import CategoryForm, ProductForm
+from .forms import CategoryForm, ProductForm, ProductImageFormSet
 
 
 class CategoryListView(LoginRequiredMixin, ListView):
@@ -40,16 +40,56 @@ class ProductListView(LoginRequiredMixin, ListView):
 
 class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
-    template_name = "product_form.html"
     form_class = ProductForm
+    template_name = "product_form.html"
     success_url = reverse_lazy("admin-product_list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            context["formset"] = ProductImageFormSet(
+                self.request.POST, self.request.FILES
+            )
+        else:
+            context["formset"] = ProductImageFormSet()
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context["formset"]
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return redirect(self.success_url)
+        return self.render_to_response(self.get_context_data(form=form))
 
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
-    template_name = "product_form.html"
     form_class = ProductForm
+    template_name = "product_form.html"
     success_url = reverse_lazy("admin-product_list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            context["formset"] = ProductImageFormSet(
+                self.request.POST, self.request.FILES, instance=self.object
+            )
+        else:
+            context["formset"] = ProductImageFormSet(instance=self.object)
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context["formset"]
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return redirect(self.success_url)
+        return self.render_to_response(self.get_context_data(form=form))
 
 
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
